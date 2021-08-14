331064a1ee23e66524192719b3d65fee9ba8a8fe
Firdaus Zahari <firdaus@fsylum.net>
2021-08-14 12:40:07 +0800

    Bump version

--- fs-login-devices.php
+++ fs-login-devices.php
@@ -3,7 +3,7 @@
  * Plugin Name: Login Devices
  * Plugin URI: https://github.com/fsylum/wp-plugin-boilerplate
  * Description: Track and display all users devices used during authentication process
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Firdaus Zahari
  * Author URI: https://fsylum.net
  * Requires at least: 5.6
@@ -15,7 +15,7 @@ require __DIR__ . '/vendor/autoload.php';
 define('FSLD_PLUGIN_URL', untrailingslashit(plugin_dir_url(__FILE__)));
 define('FSLD_PLUGIN_PATH', untrailingslashit(plugin_dir_path(__FILE__)));
 define('FSLD_PLUGIN_BASENAME', plugin_basename(__FILE__));
-define('FSLD_PLUGIN_VERSION', '1.0.0');
+define('FSLD_PLUGIN_VERSION', '1.0.1');
 
 register_activation_hook(__FILE__, [Fsylum\LoginDevices\WP\Database::class, 'install']);
 
